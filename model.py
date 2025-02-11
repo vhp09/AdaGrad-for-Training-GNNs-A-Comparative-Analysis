d2d05e9c9a27e591b3884af40c89f4df1683d785
Vishvam Patel <vish.patel00@gmail.com>
2025-02-10 20:09:53 -0600

    model definitions

---
+++ model.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GATConv, SAGEConv, GCNConv
+from torch_geometric.nn.models import GraphSAGE
+    
+class SAGE(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(SAGE, self).__init__()
+        torch.manual_seed(1234567)
+        self.conv1 = SAGEConv(in_channels, hidden_channels, aggr='mean')
+        self.conv2 = SAGEConv(hidden_channels, hidden_channels, aggr='mean')
+        self.linear = torch.nn.Linear(hidden_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.relu(self.conv2(x, edge_index))
+        x = self.linear(x)
+        return x
+
+class GCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(GCN, self).__init__()
+        torch.manual_seed(1234567)
+
+        # Initialize the layers
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        # First Message Passing Layer (Transformation)
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = F.dropout(x, p=0.5, training=self.training)
+
+        # Second Message Passing Layer
+        x = self.conv2(x, edge_index)
+        return x
